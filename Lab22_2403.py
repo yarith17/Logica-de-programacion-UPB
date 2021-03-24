9bc00ed009df69c09323d029b4e99c87c8e0a202
yarith17 <80237267+yarith17@users.noreply.github.com>
2021-03-24 17:13:52 -0500

    Add files via upload

---
+++ Lab22_2403.py
@@ -0,0 +1,9 @@
+print("EJERCICIO 1")
+for i in (10,5,-1):
+    for j in (10,5):
+        print ("i vale", i, "y j vale", j)
+
+print("EJERCICIO 2")
+for i in range(5):
+    for j in range(i):
+        print("i vale", i, "y j vale", j)
